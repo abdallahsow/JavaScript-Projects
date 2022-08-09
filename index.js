dee72e120f5b7bfe85541f1768e3c87938641090
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-09 11:53:11 +0300

    used a single function for all buttons

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-let homeScore = document.getElementById("home-score")
-let guestScore = document.getElementById("guest-score")
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
 
 let scoreHome = 0;
 let scoreGuest = 0;
 
-function add1PtHome() {
-  scoreHome += 1;
-    homeScore.textContent = scoreHome;
-    
-}
-
-function add1PtGuest() {
-  scoreGuest += 1;
-  guestScore.textContent = scoreGuest;
-}
-
-function add2PtHome() {
-  scoreHome += 2;
-  homeScore.textContent = scoreHome;
-}
-
-function add2PtGuest() {
-  scoreGuest += 2;
-  guestScore.textContent = scoreGuest;
-}
-
-function add3PtHome() {
-  scoreHome += 3;
-  homeScore.textContent = scoreHome;
-}
-
-function add3PtGuest() {
-  scoreGuest += 3;
-  guestScore.textContent = scoreGuest;
+function addPt(value, type) {
+  if (type === 'home') {
+    scoreHome += value;
+    homeScore.innerHTML = scoreHome;
+  } else {
+    scoreGuest += value;
+    guestScore.textContent = scoreGuest;
+  }
 }
 
