65238c7d563e43249c792193f0f9e2dc12662910
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-15 17:15:34 +0300

    temporary solution, seeking for better

--- Password Generator/index.js
+++ Password Generator/index.js
@@ -1,108 +1,15 @@
-let characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
-
 firstPass = document.getElementById("first-pass");
 secondPass = document.getElementById("second-pass");
 
-function generatePass() {
+let length = 15
+
+function generatePass(passLength) {
   firstPass.textContent = "";
   secondPass.textContent = "";
-  for (let i = 0; i < 15; i++) {
-    Math.floor(Math.random() * characters.length);
-    randomOnePass = characters[Math.floor(Math.random() * characters.length)];
+  for (let i = 0; i < passLength; i++) {
+    let randomOnePass = String.fromCharCode(Math.floor(Math.random() * 94) + 33);
     firstPass.textContent += randomOnePass;
-    randomTwoPass = characters[Math.floor(Math.random() * characters.length)];
+    let randomTwoPass = String.fromCharCode(Math.floor(Math.random() * 94) + 33);
     secondPass.textContent += randomTwoPass;
   }
 }
